344c9d23101f7a41caab25f1dd5b1584bf10c88c
betsy18 <betsy_dessire@hotmail.com>
2018-02-14 17:20:51 -0500

    Agregando funcionalidad

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const server = app.listen(3003, on);
+$(document).ready(function() { 
 
-function on() {
-    console.log('Server on');
-}
+  var proxy = 'https://cors-anywhere.herokuapp.com/';
+  var apiLinkDS = "https://api.darksky.net/forecast/4000565e85394e8dc859ece86fbf071f/-12.145598,%20-77.022311?lang=es";
 
-app.use(express.static('public'));
+  $.ajax({
+    url: proxy + apiLinkDS,
+    success:function(data) { console.log(data);}
+  });
 
-// clave 4000565e85394e8dc859ece86fbf071f
+});

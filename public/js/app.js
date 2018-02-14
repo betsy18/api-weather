57fa90bb2587ec9ae974f00369e5575cfdfcb177
betsy18 <betsy_dessire@hotmail.com>
2018-02-14 17:52:58 -0500

    Implementando fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,26 @@
-$(document).ready(function() { 
+window.addEventListener('load', function () {
+  let proxy = 'https://cors-anywhere.herokuapp.com/';
+  let apiLinkDS = "https://api.darksky.net/forecast/4000565e85394e8dc859ece86fbf071f/-12.145598,%20-77.022311?lang=es";
 
-  var proxy = 'https://cors-anywhere.herokuapp.com/';
-  var apiLinkDS = "https://api.darksky.net/forecast/4000565e85394e8dc859ece86fbf071f/-12.145598,%20-77.022311?lang=es";
+  fetch(proxy + apiLinkDS)
+  .then(function (response) {
+    return response.json();
+    getJson();
+  }).then(function (data) {
+    console.log(data);
+    success();
+  })
+  .catch(function (error) {
+    handleError();
+  })
 
-  $.ajax({
-    url: proxy + apiLinkDS,
-    success:function(data) { console.log(data);}
-  });
+  function success(data) {
+    console.log(data);
+    console.log('muestra datos');
+  }
+
+  function handleError() {
+    console.log('Lo malograste!, alejáte');
+  }
 
 });
